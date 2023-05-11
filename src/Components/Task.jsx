94061539d3f5257eae9e62305ea8510f79ce2241
Ashwini-noob-master <119566444+Ashwini-noob-master@users.noreply.github.com>
2023-05-11 18:06:48 +0530

    useReducer()

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import CounterOne from './CounterOne';
 
 const Task = () => {
     const params = useParams();
     const taskId = params.taskid;
     console.log(params)
   return (
-<h2>this is task with TaskId: {taskId}</h2>   
+    <>
+<h2>this is task with TaskId: {taskId}</h2>
+<CounterOne/>   
+    </>
   )
 }
 
